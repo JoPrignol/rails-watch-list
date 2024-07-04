be1e2fe0609baebfbf8022ad7692f1f2e2c1daa2
Joanny Prignol <joannyprignol@gmail.com>
2024-07-04 14:31:40 +0200

    avec commit before making a potential mistake

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  get 'lists/new', to: 'lists#new'
+  post 'lists', to: 'lists#create', as: :lists
+  get 'lists/index', to: 'lists#index'
+  get 'lists/:id', to: 'lists#show'
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
